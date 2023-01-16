b026b103fb81a326c6b84d5cd0e6a790c72dd545
T Jackson <sweetladyhazle@gmail.com>
2023-01-16 09:22:09 -0600

    Update RPSLS Project File

--- human-Child1.py
+++ human-Child1.py
@@ -4,3 +4,7 @@ class Human:
 
     def __init__(self) -> None:
         super().__init__()
+
+    def human_players(self):
+            self.human_players = input('Using number keys, Please choose between 2-5 players, How many players will be playing? ')
+            print(input)
--- player_Parent.py
+++ player_Parent.py
@@ -2,29 +2,31 @@
 class Player:
 
     def __init__(self):
-        self.number = 5
+        self.number = 1,2
         self.human_players = True
  
        
       
 
     def display_human_players(self):
-        self.human_players = input('Using number keys, Please choose between 2-5 players, How many players will be playing? ')
-        while self.human_players is True:
-            if self.number <= 5:
-                 print('Great choice! ')
-        else:
-            self.human_players = False
-            print('Please choose between 2-5 players.')
-       
-      
+        self.number = input('Using number keys, Please choose between 1 or 2 players, How many players will be playing? ')
+        if self.human_players is True:
+            if self.number != [1, 2]:
+                self.human_players = False
+                print('Please choose between 1 or 2 players')
 
+            else:
+                self.human_players is False
+                self.number == [1,2]
+                self.human_players = True
+                print('Great choice!' )
+              
 
-    # def choose_gestures(self):
-            # print('please use number keys to choose gesture selection')
-            # self.human_players = input(' Rock crushes Scissors' [1])
+    def choose_gestures(self):
+        
 
-
+        
+         print('please use number keys to choose selection')
 
     
 
